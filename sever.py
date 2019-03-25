68306dd1dc96850a7fb3aadf5f3c583c3e098b1c
borosTamas <ikuto19930802@gmail.com>
2019-03-25 15:13:40 +0100

    wrong ideas

--- sever.py
+++ sever.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def game():
+    row_num = 100
+    col_num = 50
+    return render_template('game.html', row_num=row_num, col_num=col_num)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
