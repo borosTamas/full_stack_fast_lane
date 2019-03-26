dcf050dbc3235ed89ebc0e1f10f833bf780d121f
borosTamas <ikuto19930802@gmail.com>
2019-03-26 13:26:42 +0100

    grid looks fine and has x,y coordiantes

--- sever.py
+++ sever.py
@@ -3,21 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def game():
-    row_num = 100
-    col_num = 50
-    return render_template('game.html', row_num=row_num, col_num=col_num)
-
-
-@app.route('/')
 def index():
     return render_template('index.html')
 
 
 @app.route('/game')
 def game():
-    row_num = int(request.args.get('row-num', 30))
-    col_num = int(request.args.get('col-num', 50))
+    row_num = 30
+    col_num = 10
     return render_template('game.html', row_num=row_num, col_num=col_num)
 
 
