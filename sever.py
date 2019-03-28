a5cf118560897b92b7c6afaab976c86e95bf530a
Napermial <hatvani9823@gmail.com>
2019-03-28 10:15:26 +0100

    sever changes

--- sever.py
+++ sever.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        user_name = request.form.get('user_name')
+        colour = request.form.get('fav_colour')
+        print(user_name, colour)
+        return redirect('/game')
+    return render_template('start.html')
 
+@app.route('/game')
+def game():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
