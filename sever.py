1bdadbb1455ace2d5eaa9afbd22218351e43af9f
LibLev <libardi.levente@gmail.com>
2019-03-28 20:58:14 +0100

    unfortunatelly the table printing in the html is not working....

--- sever.py
+++ sever.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    top_users = data_manager.get_top_users()
     if request.method == 'POST':
         user_name = request.form.get('user_name')
         colour = request.form.get('fav_colour')
         print(user_name, colour)
         return redirect('/game')
-    return render_template('start.html')
+    return render_template('start.html', top_users=top_users)
 
 
 @app.route('/game')
